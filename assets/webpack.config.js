983f6493a10963c90da7f90742b998d71009cf81
dsalb1 <daniel.j.salberg@gmail.com>
2023-02-13 09:46:45 -0600

    manual-webpack - complete flask webpack integration

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,15 +1,41 @@
 const path = require('path');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    styles: './src/styles.scss'
+  },
   output: {
     filename: '[name].[contenthash].js',
     publicPath: '/static/dist/',
     path: path.resolve(__dirname, '..','app', 'static', 'dist'),
     clean: true
   },
+  module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader'
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                }
+            ]
+        }]
+    },
   plugins: [
-    new WebpackManifestPlugin()
+    new WebpackManifestPlugin(),
+    new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+    }),
+    new RemoveEmptyScriptsPlugin()
   ]
 };
