040247de883d095542ac38f09cadab788e727b02
dsalb1 <daniel.j.salberg@gmail.com>
2023-03-08 22:13:37 -0600

    master - turn nginx access log back on, fix degree symbol in javascript weather API integration

--- src/js/vanilla_js.js
+++ src/js/vanilla_js.js
@@ -4,8 +4,8 @@ $(function() {
         $('.weather-res .temp').text(data.temp);
         $('.weather-res img').attr("src", data.icon);
         $('.weather-res .text').text(data.text);
-        $('.weather-res .max').text('H: ' + data.maxtemp + '&deg');
-        $('.weather-res .min').text('L: ' + data.mintemp + '&deg');
+        $('.weather-res .max').text('H: ' + data.maxtemp + '\u00B0');
+        $('.weather-res .min').text('L: ' + data.mintemp + '\u00B0');
         $('.weather-res').removeClass("hide");
     }
     $('.weather-box input[type=submit]').click(function() {
